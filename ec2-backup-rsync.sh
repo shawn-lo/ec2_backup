1d33e070dcc26d6fbc329b6de95626d1ed6c2df7
Shawn Luo <shawn.luotianyu@gmail.com>
2016-04-07 15:50:54 -0400

    create instance

--- ec2-backup-rsync.sh
+++ ec2-backup-rsync.sh
@@ -1,2 +1,24 @@
-#In Fedora
-rsync --status -h test_data/ backup/ -a
+#!/bin/sh
+
+EC2_GROUP_ID="sg-568d582e"
+KEY_NAME="asuralo-key-pair-useast1"
+IMAGE_ID="ami-0187f76b"
+
+INSTANCE_ID=""
+
+#create_ebs_volume()
+#{
+#}
+
+create_instance()
+{
+    INSTANCE_ID=$(aws ec2 run-instances --security-group-ids $EC2_GROUP_ID --key-name $KEY_NAME --image-id $IMAGE_ID | \
+        grep InstanceId | awk '{print $2}' | cut -d '"' -f 2)
+    echo 'The Instance ID is' 
+    echo $INSTANCE_ID
+}
+
+#terminate_instance()
+#{
+#}
+create_instance
