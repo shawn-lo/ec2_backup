ad553225c96c19012739d6c6deb9eae08e92cafa
Shawn Luo <shawn.luotianyu@gmail.com>
2016-04-10 17:38:14 -0400

    fix bug of using own private key

--- ec2-backup-rsync.sh
+++ ec2-backup-rsync.sh
@@ -15,8 +15,7 @@ TARGET_IP_ADDRESS=""
 INSTANCE_ID=""
 VOLUME_ID=""
 
-# Environment 
-#EC2_BACKUP_FLAGS_AWS=""
+BACKUP_METHOD="dd"
 
 get_dir_size()
 {
@@ -95,53 +94,65 @@ umount_filesystem()
 
 rsync_backup()
 {
-    rsync -avz -e ssh $EC2_BACKUP_FLAGS_SSH $SOURCE_DIR fedora@$TARGET_IP_ADDRESS:/home/fedora/backup_shell
-}
+    FLAGS="\"ssh $EC2_BACKUP_FLAGS_SSH\""
+    eval rsync -avz -e $FLAGS $SOURCE_DIR fedora@$TARGET_IP_ADDRESS:/home/fedora/backup_shell}
 
-###############
-# ice tea
-###############
-getOptions()
+usage()
 {
-    while getopts :m:v:h opt; do
-        case "$opt" in
-            m) echo "Found the -m option, with value $OPTARG";;
-            v) echo "Found the -v option, with value $OPTARG";;
-            h) echo "Found the -h option" ;;
-            *) echo "Unknown option: $opt";;
-        esac
-    done
-    shift $[ $OPTIND - 1 ]
-
-    count=1
-    for param in "$@"
-    do
-        echo "Parameter $count: $param"
-        count=$[ $count + 1 ]
-    done
+    echo "The -v should be 'dd' or 'rsync'."
 }
 
 ###############
 # main
 ###############
-#getOptions
-create_instance
-echo "[Create Instance] Done"
-create_ebs_volume
-echo "[Create Volume] Done"
-#INSTANCE_ID="i-0d37dc90"
-#TARGET_IP_ADDRESS="54.172.162.153"
-connect_instance
-#create_ebs_volume
-sleep 10
-attach_volume
-sleep 10
-mount_filesystem
-rsync_backup
-sleep 10    
+while getopts ":m:v:h" opt; do
+    case "$opt" in
+        m) 
+            echo "Found the -m option, with value $OPTARG"
+            if [ $OPTARG = "dd" ]
+            then
+                BACKUP_METHOD=$OPTARG
+                echo $BACKUP_METHOD
+            elif [ $OPTARG -eq "rsync"]
+            then
+                BACKUP_METHOD=$OPTARG
+            else
+                usage
+                exit 1
+            fi  
+        ;;
+        v) 
+            echo "Found the -v option, with value $OPTARG"
+            VOLUME_ID=$OPTARG
+        ;;
+        h) 
+            echo "Found the -h option" 
+        ;;
+        *) 
+            echo "Unknown option: $opt"
+        ;;
+    esac
+done
+shift $[ $OPTIND - 1 ]
 
-umount_filesystem
-echo "[Umount Filesystem] Done"
-sleep 1
-terminate_instance
-echo "[All Done]"
+
+#create_instance
+#echo "[Create Instance] Done"
+#create_ebs_volume
+#echo "[Create Volume] Done"
+##INSTANCE_ID="i-0d37dc90"
+##TARGET_IP_ADDRESS="54.172.162.153"
+#connect_instance
+##create_ebs_volume
+#sleep 10
+#attach_volume
+#sleep 10
+#mount_filesystem
+#rsync_backup
+#sleep 10    
+#
+#umount_filesystem
+#echo "[Umount Filesystem] Done"
+#sleep 1
+#terminate_instance
+#echo "[All Done]"
